148c8eb95cbb7a4381702093abe7f732ecef67c6
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-10 19:16:38 +0200

    fix: update node express app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(require('cors')())
 app.use('/content', express.static('content'))
 app.use('/config.json', express.static('config.json'))
+app.use('/components.json', express.static('components.json'))
 var port = process.env.PORT | 9950
 console.log("Running on port " + port)
 app.listen(port);
