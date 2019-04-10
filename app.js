3ce733f0331583a78b6d0dc6972cd38fbfda0f41
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-10 20:36:12 +0200

    feat: add component explorer html vue app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.use(require('cors')())
 app.use('/content', express.static('content'))
+app.use('/components_explorer', express.static('components_explorer'))
 app.use('/config.json', express.static('config.json'))
 app.use('/components.json', express.static('components.json'))
 var port = process.env.PORT | 9950
